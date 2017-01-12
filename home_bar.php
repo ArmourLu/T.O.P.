3a037c6be1df5cb12e16e7e09017ed9a8915cd77
root <armour.lu@gmail.com>
2017-01-12 20:39:40 +0800

    Update home bar menu

--- home_bar.php
+++ home_bar.php
@@ -1,9 +1,24 @@
+<?php
+$menu = array(
+    'home'  => array('text'=>'<span class="glyphicon glyphicon-home" aria-hidden="true">',  'url'=>'index.php'),
+    'login'  => array('text'=>'Login',  'url'=>'login.php'),
+    'register' => array('text'=>'Register', 'url'=>'register.php'),
+);
+
+?>
 <!-- Home Bar -->
 <div class="block-header infotext text-center">
 <img src="/top/image/top/logo_small.png"><br>
 <?=$ServiceName?><br>
-<a class="btn btn-default" href="index.php" role="button" title="Home"><span class="glyphicon glyphicon-home" aria-hidden="true"></span></a>
-<a class="btn btn-default" href="login.php" role="button">Login</a>
-<a class="btn btn-default" href="register.php" role="button">Register</a>
+<?php
+foreach($menu as $item){
+    if(basename($_SERVER['PHP_SELF']) == $item[url]){
+        echo '<a class="btn btn-success" href="'.$item[url].'" role="button">'.$item[text].'</span></a> ';
+    }else{
+        echo '<a class="btn btn-default" href="'.$item[url].'" role="button">'.$item[text].'</span></a> ';
+    }
+    
+}
+?>
 </div>
 <!-- Home Bar -->
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 include "/var/www/private/top_mysqli.php";
-include "/var/www/private/recaptcha.php";
 
 $sqlstr = "SELECT Value FROM sysinfo where Name='ServiceName'";
 $result = mysqli_query($conni, $sqlstr);
@@ -15,6 +14,21 @@ $ServiceName = $row[0];
 <body>
     <div class="container">
         <?php include "home_bar.php"?>
+        <div class="headertext text-center">Welcome to <?=$ServiceName?></div>
+        <div class="block-info">
+                <div class="row text-center smallinfotext">
+                    <br>
+                    <img src="/top/image/top/sersor_humity.png">&nbsp;&nbsp;&nbsp;&nbsp;
+                    <img src="/top/image/top/sersor_light.png">&nbsp;&nbsp;&nbsp;&nbsp;
+                    <img src="/top/image/top/sersor_psi.png">&nbsp;&nbsp;&nbsp;&nbsp;
+                    <img src="/top/image/top/sersor_soil.png">&nbsp;&nbsp;&nbsp;&nbsp;
+                    <img src="/top/image/top/sersor_temp.png">
+                    <br>
+                    <br>
+                    <img src="/top/image/top/TOP_Teach.png">
+                    <br><br>
+                </div>
+        </div>
     </div>
     <br><br>
 </body>
